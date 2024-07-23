64a932933216fccdffe188ac867cabae1416f9fa
Paul Madden <paul.madden@noaa.gov>
2024-07-23 04:21:10 +0000

    Update

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 set -eux
 ./INSTALL
-for x in bin lib man sbin; do
+find sbin -type f -not -name "*.sh" | xargs sed -i '1s@.*@#!/usr/bin/env ruby@'
+for x in bin gems lib man sbin; do
   rsync -av $x/ $PREFIX/$x/
 done
 cp -v VERSION $PREFIX/
